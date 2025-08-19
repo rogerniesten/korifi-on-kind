d9f66f65270ddda8eaf81e7a6c36805784dfb0b1
rogerniesten <niesten@ronits.nl>
2025-08-19 12:23:33 +0000

    Remove obsolete files and moved helper scripts to subfolder scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,17 +5,17 @@
 ##
 
 ## Includes
-scriptpath="$(dirname "${BASH_SOURCE[0]}")"
-. "$scriptpath/utils.sh"
+. env/.env || { echo "Config ERROR! Script aborted"; exit 1; }                  # read paths from environment file
+. "$LIB_PATH/cf_utils.sh"
 
 
 ##
 ## Config
 ##
 export K8S_TYPE=KIND     					# type: KIND, AKS
-prompt_if_missing K8S_TYPE "var" "Which K8S type to use? (KIND, AKS)"
 prompt_if_missing K8S_CLUSTER_KORIFI "var" "Name of K8S Cluster for Korifi"
-. .env || { echo "Config ERROR! Script aborted"; exit 1; }	# read config from environment file
+. "$ENV_PATH/.env.korifi" || { echo "Config ERROR! Script aborted"; exit 1; }   # read korifi config from environment file
+
 read -r -p "Press enter to continue or CTRL-C to abort"
 
 strongly_advice_root
@@ -25,14 +25,12 @@ strongly_advice_root
 ## Cleanup
 ##
 echo "cleanup..."
-#if $($SUDOCMD kind get clusters | grep 'korifi'); then
 echo "DBG: $SUDOCMD kind delete clusters \"${K8S_CLUSTER_KORIFI}\""
 $SUDOCMD kind delete clusters "${K8S_CLUSTER_KORIFI}"
 
 $SUDOCMD kubectl config delete-user "cf-admin@${K8S_CLUSTER_KORIFI}"
 $SUDOCMD kubectl config delete-context "cf-admin@${K8S_CLUSTER_KORIFI}"
 
-#fi
 rm ~/.cf -rf
 rm ~/.kube/certs -rf
 rm tmp -rf
